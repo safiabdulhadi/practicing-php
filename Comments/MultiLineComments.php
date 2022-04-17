d94e77118c108b006cd4536c818183fd9dabfcbe
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-04-17 16:27:02 +0200

    add mutiLineComments in php

---
+++ Comments/MultiLineComments.php
@@ -0,0 +1,16 @@
+
+<?php
+/*
+This program is used to understand the types of comments in php.
+*/
+
+?>
+
+<?= "PHP is Intersting"; /* Comment are fine here */ ?>
+
+<?php
+/* 
+echo "Welcome to PHP! // This is a Echo Method 
+*/
+
+?>
